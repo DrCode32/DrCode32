69c593678bc1c8e45e71731f7506b7b686353fca
Dr.Code <87318660+DrCode32@users.noreply.github.com>
2025-02-02 21:31:36 +0330

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,7 @@ const SCROLL_LOCK_DELAY = 2800;
 const FADE_DELAY = 500;
 const PAGE_TRANSITION_DELAY = 1200;
 const BACK_TRANSITION_DELAY = 2000;
-const JOKE_SCRIPT = `What do you call a fake noodle?
-
-
-
-An impasta!`;
+const JOKE_SCRIPT = `What do you call a fake noodle?\r\n\r\n\r\n\r\nAn impasta!`;
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
